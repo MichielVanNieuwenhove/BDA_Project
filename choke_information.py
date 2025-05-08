411cf7814574509957933c1bfac8555608a29d54
MichielVanNieuwenhove <vannieuwenhovemichiel@gmail.com>
2025-05-08 16:19:41 +0200

    Update choke_information.py

--- choke_information.py
+++ choke_information.py
@@ -1,7 +1,16 @@
 
 class ChokeInformation:
-    def __init__(self):
+    def __init__(self, time):
+        self.uploaded_bytes = 0
+        self.last_update_time = time
         self.upload_rate = 0
 
     def add_upload(self, time):
-        ...
+        time_elapsed = time - self.last_update_time
+        if time_elapsed > 0:
+            self.upload_rate = self.uploaded_bytes / time_elapsed
+        else:
+            self.upload_rate = self.upload_rate
+
+        self.uploaded_bytes = 1
+        self.last_update_time = time
